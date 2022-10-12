8d7a91dc2ada4fae29591392893cc5882e2e351e
Yasserhub <yasser.a.murshed@gmail.com>
2022-10-12 12:06:45 +0200

    creating button for question's answers

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -4,8 +4,18 @@
  */
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  // element.innerHTML = String.raw`
+  //   ${key}: ${answerText};
+  // `;
   element.innerHTML = String.raw`
-    ${key}: ${answerText};
-  `;
+    <button class="answers" id=${key}>${key}: ${answerText}</button>
+`;
   return element;
+};
+
+export const answerButtonDisable = () => {
+  const buttonDeActivation= document.querySelectorAll('.answers');
+  buttonDeActivation.forEach((buttonDeactive) => {
+   buttonDeactive.disabled=true;
+  });
 };
