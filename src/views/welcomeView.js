c3020b330a56223ea8f062f3560c837c04e46ce5
Arkaitz Garro <arkaitz.garro@gmail.com>
2022-10-15 20:44:34 +0200

    Style welcome page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -6,8 +6,9 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  */
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
+  element.classList.add('welkome');
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
+    <h1 class="welc">Welcome</h1>
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
   `;
   return element;
