434e8fa1f91f73bf28f8d37041cb3863ae72aa0c
SvitlanaMOliinyk <s.m.oliinyk@gmail.com>
2022-10-15 17:34:30 +0200

    Added local storage clear, score storage and styling

--- src/views/selectedAnswerView.js
+++ src/views/selectedAnswerView.js
@@ -5,6 +5,7 @@ export const selectAnswerVariant = (selectedElement, rightAnswer) => {
   if (selectedElement === rightAnswer) {
     selectedElement.classList.add('selected-green');
     quizData.finalScore++;
+    localStorage.setItem('scoreNumber', JSON.stringify(quizData.finalScore));
   } else {
     selectedElement.classList.add('selected-red');
     rightAnswer.classList.add('border');
