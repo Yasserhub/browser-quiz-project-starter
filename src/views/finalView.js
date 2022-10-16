790372785737b103f523893471a452a43590f943
Arkaitz Garro <arkaitz.garro@gmail.com>
2022-10-16 11:33:47 +0200

    remove repetition

--- src/views/finalView.js
+++ src/views/finalView.js
@@ -6,41 +6,21 @@ export const createFinalSummaryElement = () => {
   const percentage = (finalScore / questions.length) * 100;
   const totalScore = (finalScore + "  /  " + questions.length);
 
-  if (quizData.finalScore <= 3) {
-    const element = document.createElement('div');
-    element.classList.add('result-box');
-    element.innerHTML = `
-      <h1 class='final'> Unfortunately! </h1> <br><h2> You were awful :( </h2><br>
-      <table>
-          <tr>
-              <td>Total Question</td>
-              <td span class="total-question"></span>${questions.length}</td>
-          </tr>
-          <tr>
-              <td>Correct</td>
-              <td span class="total-question"></span>${finalScore}</td>
-          </tr>
-          <tr>
-              <td>Wrong</td>
-              <td span class="total-question"></span>${wrongAnswers}</td>
-          </tr>
-              <td>Percentage</td>
-              <td span class="total-question"></span>${percentage.toFixed(2) + "%"}</td>
-          </tr>
-          </tr>
-              <td>Total Score</td>
-              <td span class="total-question"></span>${totalScore}</td>
-          </tr>
-      </table>
-        `;
-    return element;
+  const element = document.createElement('div');
+  element.classList.add('result-box');
 
-  } if (quizData.finalScore <= 5) {
-    const element = document.createElement('div');
-    element.classList.add('result-box');
-    element.innerHTML = `
-        <h1 class='final'> Good! </h1><br><h2> You need to Work hard :/ </h2><br> 
-      <table>
+  let title = ''
+  if (quizData.finalScore <= 3) {
+    title = '<h1 class="final"> Unfortunately! </h1> <br><h2> You were awful :( </h2><br>'
+  } else if (quizData.finalScore <= 5) {
+    title = '<h1 class="final"> Good! </h1><br><h2> You need to Work hard :/ </h2><br>'
+  } else {
+    title = '<h1 class="final"> Congratulations! </h1><br><h2> You are a hero :)</h2><br> '
+  }
+
+  element.innerHTML = `
+    ${title}
+    <table>
         <tr>
             <td>Total Question</td>
             <td span class="total-question"></span>${questions.length}</td>
@@ -61,37 +41,7 @@ export const createFinalSummaryElement = () => {
             <td span class="total-question"></span>${totalScore}</td>
         </tr>
     </table>
-        `;
-    return element;
+      `;
 
-  } else {
-    const element = document.createElement('div');
-    element.classList.add('result-box');
-    element.innerHTML = `
-        <h1 class='final'> Congratulations! </h1><br><h2> You are a hero :)</h2><br> 
-      <table>
-        <tr>
-            <td>Total Question</td>
-            <td span class="total-question"></span>${questions.length}</td>
-        </tr>
-        <tr>
-            <td>Correct</td>
-            <td span class="total-question"></span>${finalScore}</td>
-        </tr>
-        <tr>
-            <td>Wrong</td>
-            <td span class="total-question"></span>${wrongAnswers}</td>
-        </tr>
-            <td>Percentage</td>
-            <td span class="total-question"></span>${percentage.toFixed(2) + "%"}</td>
-        </tr>
-        </tr>
-            <td>Total Score</td>
-            <td span class="total-question"></span>${totalScore}</td>
-        </tr>
-      </table>
-        `;
-    return element;
-  }
-
+  return element;
 };
